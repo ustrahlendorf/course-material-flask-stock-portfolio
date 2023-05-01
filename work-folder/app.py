7519f40c897df7b7eaed8fbca7b2a633e5aaf2c3
Uwe Strahlendorf <ustrahlendorf@tecracer.de>
2023-05-01 18:35:29 +0200

    Initial commit part-1 content

---
+++ work-folder/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, escape
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Hello World!'
+
+@app.route('/about')
+def about():
+    return '<h2>About this application...</h2>'
+
+@app.route('/stocks/')
+def stocks():
+    return '<h2>Stock List...</h2>'
+
+@app.route('/hello/<message>')
+def hello_message(message):
+    return f'<h1>Welcome {escape(message)}!</h1>'
+
+@app.route('/blog_posts/<int:post_id>')
+def display_blog_post(post_id):
+    return f'<h1>Blog Post #{post_id}...</h1>'
+
+#@app.route('/example', methods=['GET', 'POST'])
+#def example():
+#    return 'an example'
